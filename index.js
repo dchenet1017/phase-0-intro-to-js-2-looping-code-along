a1f750149f46191681257febef7b431f78288f5a
Dimitri Chenet <dchenet1017@gmail.com>
2021-09-13 21:33:47 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+
+
+
+const occasions = ["Happy birthday","Happy Annivsary", " Merry Christmas"];
+function writeCards(occasions) {
+    for (let i=0; i< occasions.length; i++) {
+        console.log(`${occasions [i]}to you and your family!`);
+        debugger;
+    }
+    return occasions;
+}
+writeCards(occasions)
+
+
+function countDown (startingNUmber) {
+    while (startingNumber> 0) {
+        console.log (startingNymber );
+        startingNumber -=1;
+    }
+    console.log(startingNumber);
+}
